c578d6bf06566f3b86e3aa9e28fd9c93d71f836b
Denis Bystruev <dbystruev@me.com>
2019-02-02 16:16:23 +0300

    Initial Commit

---
+++ Apple Pie/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Apple Pie
+//
+//  Created by Denis Bystruev on 02/02/2019.
+//  Copyright © 2019 Denis Bystruev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var topStackView: UIStackView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        
+        if size.width < size.height {
+            topStackView.axis = .vertical
+        } else {
+            topStackView.axis = .horizontal
+        }
+    }
+
+
+}
+
