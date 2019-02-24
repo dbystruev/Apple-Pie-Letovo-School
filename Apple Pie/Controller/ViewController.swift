5ebb86ac683bc5bb09a1e55f7ca06a73a5957c9d
Denis Bystruev <dbystruev@me.com>
2019-02-24 12:22:29 +0300

    Finished the project

--- Apple Pie/Controller/ViewController.swift
+++ Apple Pie/Controller/ViewController.swift
@@ -19,10 +19,21 @@ class ViewController: UIViewController {
     // MARK: - ... Properties
     var currentGame: Game!
     
+    var finished = false
+    
     let incorrectMovesAllowed = 7
     
-    var totalWins = 0
-    var totalLosses = 0
+    var totalWins = 0 {
+        didSet {
+            newRound()
+        }
+    }
+    
+    var totalLosses = 0 {
+        didSet {
+            newRound()
+        }
+    }
 
     // MARK: - ... Methods
     override func viewDidLoad() {
@@ -40,8 +51,31 @@ class ViewController: UIViewController {
         }
     }
     
+    func enable(view: UIView, isEnabled: Bool) {
+        if let button = view as? UIButton {
+            button.isEnabled = isEnabled
+        }
+        
+        for subview in view.subviews {
+            enable(view: subview, isEnabled: isEnabled)
+        }
+    }
+    
     func newRound() {
         let count = Game.listOfWords.count
+        
+        guard 0 < count else {
+            correctWordLabel.text = "Игра окончена"
+            
+            enable(view: view, isEnabled: false)
+            
+            finished = true
+            
+            return
+        }
+        
+        enable(view: view, isEnabled: true)
+        
         let index: Int
         
         if count < 2 {
@@ -62,6 +96,8 @@ class ViewController: UIViewController {
     }
     
     func updateUI() {
+        guard !finished else { return }
+        
         let imageName = "Tree \(currentGame.incorrectMovesRemaining)"
         let image = UIImage(named: imageName)
         
@@ -72,6 +108,15 @@ class ViewController: UIViewController {
         scoreLabel.text = "Выигрыши: \(totalWins), проигрыши: \(totalLosses)"
     }
     
+    func updateGameState() {
+        if currentGame.incorrectMovesRemaining < 1 {
+            totalLosses += 1
+        } else if currentGame.correctWord == currentGame.word.lowercased() {
+            totalWins += 1
+        }
+        updateUI()
+    }
+    
     // MARK: - ... IBAction
     @IBAction func buttonPressed(_ sender: UIButton) {
         sender.isEnabled = false
@@ -79,6 +124,8 @@ class ViewController: UIViewController {
         let letter = sender.title(for: .normal)!
         
         currentGame.guess(letter: letter)
+        
+        updateGameState()
     }
 }
 
